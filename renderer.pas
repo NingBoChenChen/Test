19e0894c27fb097d2427d14f6006f8fcd10734b2
NingBoChenChen <chenchen20050719@126.com>
2018-08-25 08:27:31 +0800

    1
    1

--- renderer.pas
+++ renderer.pas
@@ -1,9 +1,21 @@
 library renderer;
 
 uses
-	gl,glu,glut;
+	gl;
 
 type
 	TRenderer=class
-		
+		private
+			Textured,Colored:boolean;
+			Vertexes,vp,tp,cp:longint;
+			VertexArray,TexcoordArray,ColorArray:^GLFloat;
+			TexcoordX,TexcoordY:GLFloat;
+			ColorR,ColorG,ColorB:GLFloat;
+		public
+			procedure Init;
+			procedure Vertex3f(x,y,z:GLFloat);
+			procedure TexCoord2f(x,y:GLFloat);
+			procedure Color3f(r,g,b:GLFloat);
+			procedure Flush;
 	end;
+
